f228292bc42e008ffc8081b4ffef47afdd067b1c
Dario Junior <106486127+Drario11@users.noreply.github.com>
2024-02-14 18:17:55 -0300

    em andamento

--- App.js
+++ App.js
@@ -1,20 +1,71 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import{useState} from 'react'
+import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native'
+import Slider from '@react-native-community/slider'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default function App(){
+  const [size, setSize] = useState(10)
+
+  return(
+    <View style={style.continer}>
+      <Image
+        source={require("./src/assets/logo.png")}
+        style={style.logo}
+      />
+      <Text style={style.title}>{size} caracteres</Text>
+
+      <View style={style.area}>
+        <Slider
+          style={{ height: 50}}
+          minimumValue={6}
+          maximumValue={20}
+          maximumTrackTintColor='#ff0000'
+          minimumTrackTintColor='#000'
+          value={size}
+          onValueChange={ (value)=> setSize(value.toFixed(0))}
+        />
+      </View>
+      <TouchableOpacity style={style.button}>
+        <Text style={style.buttonText}>Entrar Site</Text>
+      </TouchableOpacity>
+    </View> 
+
+  )  
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+const style = StyleSheet.create({
+  continer:{
+    flex:1,
+    backgroundColor:'#F3F3FF',
     justifyContent: 'center',
+    alignItems:'center'    
   },
-});
+  logo:{
+    marginBottom:60
+  },
+  area:{
+    marginTop:14,
+    marginBottom:14,
+    width:"80%",
+    backgroundColor:'#FFF',
+    borderRadius: 10,
+    padding: 5,
+  },
+  button:{
+    backgroundColor: '#392de9',
+    width:'80%',
+    height: 50,
+    justifyContent:'center',
+    alignItems:'center',
+    borderRadius:8,
+    marginBottom:18,
+  },
+  buttonText:{
+    color:'#FFF',
+    fontSize:20,
+    
+  },
+  title:{
+    fontSize:30,
+    fontWeight:'bold',
+  }
+})
